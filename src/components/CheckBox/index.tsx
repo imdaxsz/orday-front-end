d11baa1c89ca8a1129386b80eff19bc65db55827
lyeejj <lyshine012@gmail.com>
2023-09-25 11:29:01 +0900

    feat: CheckBox 커스텀 및 type props 추가 설정

--- src/components/CheckBox/index.tsx
+++ src/components/CheckBox/index.tsx
@@ -3,9 +3,10 @@ import { CheckboxContainer, CheckboxInput, CheckboxText } from "./style";
 interface CheckBoxProps {
   text?: string;
   onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  type?: string;
 }
 
-export function CheckBox({ text, onChange }: CheckBoxProps) {
+export function CheckBox({ text, onChange, type }: CheckBoxProps) {
   return (
     <CheckboxContainer>
       <CheckboxInput
@@ -13,6 +14,7 @@ export function CheckBox({ text, onChange }: CheckBoxProps) {
         id={text}
         name={text}
         onChange={onChange}
+        isCircle={type === "circle"}
       />
       <CheckboxText htmlFor={text}>{text}</CheckboxText>
     </CheckboxContainer>
