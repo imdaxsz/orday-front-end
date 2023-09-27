da1248a55d9b43250cc7234ce35c395963cc1ee3
lyeejj <lyshine012@gmail.com>
2023-09-27 23:51:52 +0900

    design: Dropdown 스타일 변경

--- src/components/Dropdown/style.ts
+++ src/components/Dropdown/style.ts
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const DropdownBox = styled.div`
   position: relative;
-  width: 7.5rem;
+  width: 8rem;
   padding: 0.5rem;
   border-radius: 12px;
   background-color: transparent;
@@ -11,11 +11,10 @@ export const Label = styled.label`
   display: flex;
   align-items: center;
   justify-content: space-between;
-  font-size: 14px;
   color: #5d5d5d;
   cursor: pointer;
 `;
-export const DropdownOptions = styled.ul<{ show: boolean }>`
+export const DropdownOptions = styled.ul<{ $show: boolean }>`
   position: absolute;
   top: 45px;
   left: 0;
@@ -25,12 +24,11 @@ export const DropdownOptions = styled.ul<{ show: boolean }>`
   border-radius: 10px;
   background-color: ${({ theme }) => theme.colors.primary["60"]};
   color: #fff;
-  max-height: ${(props) => (props.show ? "none" : "0")};
+  max-height: ${({ $show }) => ($show ? "none" : "0")};
   z-index: 100;
 `;
 export const Option = styled.li`
   padding: 0.5rem 1rem;
-  font-size: 14px;
   cursor: pointer;
   &:hover {
     background-color: ${({ theme }) => theme.colors.primary["80"]};
