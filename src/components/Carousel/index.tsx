904cb5866e56ab169e3c9536cf680053ed089e3d
mihee <s2809h29@gmail.com>
2023-09-27 15:54:46 +0900

    feat: 랜딩 페이지 Card Carousel 구현 #15

---
+++ src/components/Carousel/index.tsx
@@ -0,0 +1,93 @@
+import { useEffect, useState } from "react";
+import { IoIosArrowForward, IoIosArrowBack } from "react-icons/io";
+import { Link } from "react-router-dom";
+
+import { ArrowIconButton } from "../VisualSection/style";
+
+import { Card, Container, Info, SlideContainer, Slider } from "./style";
+
+interface CarouselItem {
+  image: string;
+  url: string;
+  info: {
+    title: string;
+    content: string;
+  };
+}
+
+export default function Carousel({ items }: { items: CarouselItem[] }) {
+  const [currentIndex, setCurrentIndex] = useState(3);
+  const [translateX, setTranslateX] = useState(0);
+  const [transition, setTransition] = useState(0.5);
+  const [info, setInfo] = useState(items[1].info);
+
+  const SLIDE_RESET_DELAY = 500;
+  const CHANGE_INFO_DELAY = 500; // 이미지가 완전히 교체된 후 info가 수정되도록 합니다.
+
+  const carouselItems = [
+    items[items.length - 2],
+    items[items.length - 1],
+    ...items,
+    items[0],
+    items[1],
+  ];
+
+  const handlePrevClick = () => {
+    setCurrentIndex((prevIndex) => (prevIndex === 0 ? 2 : prevIndex - 1));
+    setTransition(0.5);
+    setTranslateX((prev) => prev + 430);
+  };
+
+  const handleNextClick = () => {
+    setCurrentIndex((prevIndex) => prevIndex + 1);
+    setTransition(0.5);
+    setTranslateX((prev) => prev - 430);
+    setTimeout(() => {
+      setInfo({ ...carouselItems[currentIndex].info });
+    }, CHANGE_INFO_DELAY);
+  };
+
+  useEffect(() => {
+    if (currentIndex === 1) {
+      setTimeout(() => {
+        setCurrentIndex(4);
+        setTranslateX(-430);
+        setTransition(0);
+      }, SLIDE_RESET_DELAY);
+    } else if (currentIndex === 5) {
+      setTimeout(() => {
+        setCurrentIndex(2);
+        setTranslateX(430);
+        setTransition(0);
+      }, SLIDE_RESET_DELAY);
+    }
+  }, [currentIndex]);
+
+  return (
+    <Container>
+      <SlideContainer>
+        <ArrowIconButton onClick={handlePrevClick} style={{ left: "310px" }}>
+          <IoIosArrowBack />
+        </ArrowIconButton>
+        <ArrowIconButton onClick={handleNextClick} style={{ right: "310px" }}>
+          <IoIosArrowForward />
+        </ArrowIconButton>
+        <Slider $translateX={translateX} $transition={transition}>
+          {carouselItems.map((item, i) => (
+            <Link to={item.url} key={i}>
+              <Card
+                src={item.image}
+                $active={i === currentIndex}
+                $transition={transition}
+              />
+            </Link>
+          ))}
+        </Slider>
+      </SlideContainer>
+      <Info to={carouselItems[currentIndex].url}>
+        <h3>{info.title}</h3>
+        <span>{info.content}</span>
+      </Info>
+    </Container>
+  );
+}
---
+++ src/components/Carousel/style.ts
@@ -0,0 +1,70 @@
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+interface SliderProps {
+  $translateX: number;
+  $transition: number;
+}
+
+export const Container = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 20px;
+`;
+
+export const SlideContainer = styled.div`
+  width: 100%;
+  height: 350px;
+  overflow: hidden;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  svg {
+    color: ${({ theme }) => theme.colors["neutral"]["90"]};
+    transform: scale(0.9);
+  }
+`;
+
+export const Slider = styled.div<SliderProps>`
+  width: fit-content;
+  height: 350px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 30px;
+  transform: ${({ $translateX = 0 }) => `translateX(${$translateX}px)`};
+  transition: all ${({ $transition = 0.5 }) => `${$transition}s`};
+`;
+
+export const Card = styled.img<{ $active?: boolean; $transition?: number }>`
+  display: block;
+  width: ${({ $active = false }) => ($active ? "500px" : "400px")};
+  height: ${({ $active = false }) => ($active ? "350px" : "300px")};
+  filter: brightness(${({ $active = false }) => ($active ? "100%" : "70%")});
+  background: ${({ theme }) => theme.colors["neutral"]["10"]};
+  flex-shrink: 0;
+  border-radius: 20px;
+  object-fit: cover;
+  transition: all ${({ $transition = 0.5 }) => `${$transition}s`};
+`;
+
+export const Info = styled(Link)`
+  width: 500px;
+  display: flex;
+  flex-direction: column;
+  padding-left: 10px;
+  gap: 7px;
+  & > h3 {
+    color: ${({ theme }) => theme.colors["neutral"]["100"]};
+    ${({ theme }) => theme.typo["title-2-b"]};
+    line-height: 170%;
+  }
+
+  & > span {
+    color: ${({ theme }) => theme.colors["neutral"]["70"]};
+    ${({ theme }) => theme.typo["body-2-r"]};
+  }
+`;
