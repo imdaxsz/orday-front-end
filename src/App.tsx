ea1cbdb52faa841bd9c9fdcffcf0fdaae65a4257
mihee <s2809h29@gmail.com>
2023-09-30 22:49:54 +0900

    feat: 브랜드 페이지 라우터 추가

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import { ThemeProvider } from "styled-components";
 import theme from "@/styles/theme";
 
 import Layout from "./components/Layout";
+import BrandDetail from "./routes/Brand/Detail";
+import BrandList from "./routes/Brand/List";
 import Home from "./routes/Home";
 import GlobalStyle from "./styles/GlobalStyle";
 
@@ -15,6 +17,8 @@ export default function App() {
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route path="" element={<Home />} />
+            <Route path="brands" element={<BrandList />} />
+            <Route path="brands/:name" element={<BrandDetail />} />
           </Route>
         </Routes>
       </ThemeProvider>
