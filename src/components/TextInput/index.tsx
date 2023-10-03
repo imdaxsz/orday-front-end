6f5465eec6b3bcf7d3ac5fa34a2a2d2239617f9b
mihee <s2809h29@gmail.com>
2023-10-03 18:42:44 +0900

    feat: TextInput #31

---
+++ src/components/TextInput/index.tsx
@@ -0,0 +1,28 @@
+import { ComponentProps } from "react";
+
+import { Container, Input } from "./style";
+
+interface TextInputProps extends ComponentProps<"input"> {
+  label?: string;
+  id: string;
+  warn?: boolean;
+  message?: string; // 오류 메시지
+}
+
+export default function TextInput({
+  label,
+  id,
+  warn,
+  message,
+  className,
+  style,
+  ...props
+}: TextInputProps) {
+  return (
+    <Container className={className} style={style}>
+      {label && <label htmlFor={id}>{label}</label>}
+      <Input id={id} {...props} />
+      {warn && message && <span>{message}</span>}
+    </Container>
+  );
+}
---
+++ src/components/TextInput/style.ts
@@ -0,0 +1,35 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  & > label {
+    display: block;
+    ${({ theme }) => theme.typo["body-3-r"]};
+    color: ${({ theme }) => theme.colors["neutral"]["70"]};
+    line-height: 120%;
+    margin-bottom: 5px;
+  }
+
+  & > span {
+    color: red;
+    ${({ theme }) => theme.typo["body-4-r"]};
+    margin-top: 5px;
+  }
+`;
+
+export const Input = styled.input`
+  height: 50px;
+  padding: 18px 20px;
+  border-radius: 10px;
+  background: white;
+  color: ${({ theme }) => theme.colors["neutral"]["100"]};
+  border: solid 1px ${({ theme }) => theme.colors["neutral"]["40"]};
+
+  &::placeholder {
+    color: ${({ theme }) => theme.colors["neutral"]["40"]};
+  }
+
+  &:focus {
+    outline: none;
+    border-color: ${({ theme }) => theme.colors["neutral"]["70"]};
+  }
+`;
