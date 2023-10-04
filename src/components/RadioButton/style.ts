78c4f374609f0f7a428d6094e74fd71dc0f79e5d
lyeejj <lyshine012@gmail.com>
2023-10-04 23:03:38 +0900

    feat: RadioButton 커스텀 스타일 적용

--- src/components/RadioButton/style.ts
+++ src/components/RadioButton/style.ts
@@ -13,16 +13,23 @@ export const RadioText = styled.label`
 `;
 
 export const RadioInput = styled.input`
-  margin-left: 0.5rem;
+  appearance: none;
   width: 1rem;
   height: 1rem;
-  accent-color: ${({ theme }) => theme.colors.primary["80"]};
+  border-radius: 50%;
+  box-shadow: 0 0 0 1px ${({ theme }) => theme.colors.neutral["40"]};
   &:hover {
     cursor: pointer;
   }
+  &:checked {
+    border: 3px solid #fff;
+    border-radius: 50%;
+    box-shadow: 0 0 0 1px ${({ theme }) => theme.colors.primary["80"]};
+    background-color: ${({ theme }) => theme.colors.primary["80"]};
+  }
   &:disabled {
-    background-color: ${({ theme }) => theme.colors.neutral["40"]};
-    opacity: 0.8;
+    background-color: ${({ theme }) => theme.colors.neutral["20"]};
+    box-shadow: 0 0 0 1px ${({ theme }) => theme.colors.neutral["20"]};
     cursor: not-allowed;
   }
 `;
