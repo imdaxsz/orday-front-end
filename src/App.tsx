542704f8ba12866b0adcb7771f172d373dbd8edd
lyeejj <lyshine012@gmail.com>
2023-10-09 17:34:55 +0900

    feat: 비회원 route 추가

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ import Home from "./routes/Home";
 import Join from "./routes/Join";
 import Login from "./routes/Login";
 import Order from "./routes/Order";
+import GuestOrder from "./routes/Order/GuestOrder";
 import GlobalStyle from "./styles/GlobalStyle";
 
 export default function App() {
@@ -25,6 +26,7 @@ export default function App() {
             <Route path="brands/:name" element={<BrandDetail />} />
             <Route path="category" element={<Category />} />
             <Route path="order" element={<Order />} />
+            <Route path="order/guest" element={<GuestOrder />} />
           </Route>
           <Route path="/login" element={<Login />} />
           <Route path="/login/guestOrder" element={<Login />} />
