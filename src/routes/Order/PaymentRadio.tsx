db5ec6176e562b986ed7adbd216a838cb3468759
lyeejj <lyshine012@gmail.com>
2023-10-10 17:41:14 +0900

    fix: 변수명 변경

--- src/routes/Order/PaymentRadio.tsx
+++ src/routes/Order/PaymentRadio.tsx
@@ -4,10 +4,10 @@ import { styled } from "styled-components";
 import Button from "@/components/Button";
 
 export default function PaymentRadio() {
-  const [selectedButton, setSelectedButton] = useState<number | null>(null);
+  const [selectedMethod, setSelectedMethod] = useState<number | null>(null);
 
   const handleButtonClick = (index: number) => {
-    setSelectedButton(index);
+    setSelectedMethod(index);
   };
 
   return (
@@ -15,19 +15,19 @@ export default function PaymentRadio() {
       <h3>결제수단 선택</h3>
       <PaymentGroup>
         <Button
-          color={selectedButton === 0 ? undefined : "neutral"}
+          color={selectedMethod === 0 ? undefined : "neutral"}
           onClick={() => handleButtonClick(0)}
         >
           카드
         </Button>
         <Button
-          color={selectedButton === 1 ? undefined : "neutral"}
+          color={selectedMethod === 1 ? undefined : "neutral"}
           onClick={() => handleButtonClick(1)}
         >
           계좌이체
         </Button>
         <Button
-          color={selectedButton === 2 ? undefined : "neutral"}
+          color={selectedMethod === 2 ? undefined : "neutral"}
           onClick={() => handleButtonClick(2)}
         >
           카카오페이
