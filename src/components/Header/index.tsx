ab90ba2d6270915dcd6a772000f520a580a5ba98
mihee <s2809h29@gmail.com>
2023-10-12 16:13:38 +0900

    refactor: Header 컴포넌트 코드 분리 #7

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,56 +1,31 @@
 import { useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
-import styled from "styled-components";
 
 import Logo from "@/assets/logo.svg?react";
 
-import Menu from "./Menu";
+import Backdrop from "../Backdrop";
+import Menu from "../Menu";
 
-const Container = styled.div<{ $hasBorder: boolean }>`
-  width: 100%;
-  height: 70px;
-  background-color: white;
-  padding: 15px 30px;
-  border-bottom: solid 1px
-    ${({ $hasBorder, theme }) =>
-      $hasBorder ? "white" : theme.colors["primary"]["60"]};
-  position: relative;
-`;
-
-const Content = styled.div`
-  width: 100%;
-  height: 100%;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  svg {
-    display: block;
-  }
-`;
-
-const Group = styled.ul`
-  display: flex;
-  align-items: center;
-  gap: 10px;
-`;
-
-const Item = styled.li`
-  float: left;
-  cursor: pointer;
-  ${({ theme }) => theme.typo["body-2-r"]};
-  letter-spacing: -0.16px;
-`;
+import MyMenu from "./MyMenu";
+import { Container, Content, Group, Item } from "./style";
 
 export default function Header() {
   const [menuIsVisible, setMenuIsVisible] = useState(false);
+  const [myMenuIsVisible, setMyMenuIsVisible] = useState(false);
   const location = useLocation();
   const pathname = location.pathname.split("/");
   // 랜딩 페이지, 브랜드 상세 페이지에서만 border bottom 제거
   const hasBorder =
     pathname[1] === "" || (pathname[1] === "brands" && Boolean(pathname[2]));
 
+  const handleMyClick = () => {
+    // TODO: 로그인 상태 확인
+    setMyMenuIsVisible(true);
+  };
+
   useEffect(() => {
     setMenuIsVisible(false);
+    setMyMenuIsVisible(false);
   }, [location]);
 
   return (
@@ -58,18 +33,33 @@ export default function Header() {
       <Content>
         <Group>
           <Item onClick={() => setMenuIsVisible((prev) => !prev)}>SHOP</Item>
-          <Item>COMMUNITY</Item>
+          <Item>
+            <Link to="/community">COMMUNITY</Link>
+          </Item>
         </Group>
         <Link to="/">
           <Logo />
         </Link>
         <Group>
           <Item>SEARCH</Item>
-          <Item>BAG</Item>
-          <Item>MY</Item>
+          <Item>
+            <Link to="/cart">CART</Link>
+          </Item>
+          <Item onClick={handleMyClick}>MY</Item>
         </Group>
       </Content>
-      {menuIsVisible && <Menu />}
+      {menuIsVisible && (
+        <>
+          <Backdrop onClick={() => setMenuIsVisible(false)} />
+          <Menu />
+        </>
+      )}
+      {myMenuIsVisible && (
+        <>
+          <Backdrop onClick={() => setMyMenuIsVisible(false)} />
+          <MyMenu />
+        </>
+      )}
     </Container>
   );
 }
