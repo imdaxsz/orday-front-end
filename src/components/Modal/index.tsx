a019883e11d5335c6439069f56e093cde1b317bc
lyeejj <lyshine012@gmail.com>
2023-10-15 15:54:52 +0900

    fix: Modal 컴포넌트 스타일 import #52

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,6 +1,12 @@
-import styled from "styled-components";
-
 import { StrictPropsWithChildren } from "@/types";
+
+import {
+  ButtonBox,
+  CancelBtn,
+  ConfirmBtn,
+  ModalContainer,
+  ModalContent,
+} from "./style";
 
 interface ModalProps extends StrictPropsWithChildren {
   isOpen: boolean;
@@ -30,49 +36,3 @@ export default function Modal({
     </ModalContainer>
   );
 }
-
-const ModalContainer = styled.div<{ $isOpen: boolean }>`
-  display: ${({ $isOpen }) => ($isOpen ? "block" : "none")};
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background-color: rgba(0, 0, 0, 0.2);
-  z-index: 5;
-`;
-
-const ModalContent = styled.div`
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  width: 500px;
-  height: auto;
-  background-color: #fff;
-  padding: 20px;
-  border-radius: 8px;
-  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.2);
-`;
-
-const ButtonBox = styled.div`
-  margin: 28px 0 45px 0;
-  display: flex;
-  justify-content: center;
-  gap: 8px;
-`;
-const ConfirmBtn = styled.button`
-  width: 100px;
-  height: 34px;
-  border: none;
-  border-radius: 5px;
-  background-color: ${({ theme }) => theme.colors.primary["80"]};
-  color: #fff;
-`;
-const CancelBtn = styled.button`
-  width: 100px;
-  height: 34px;
-  border-radius: 5px;
-  border: 1px solid #9d9d9d;
-  background-color: ${({ theme }) => theme.colors.neutral["10"]};
-`;
