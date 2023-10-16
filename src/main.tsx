75f7aa96b53d880420c6066711881af800dc2c43
mihee <s2809h29@gmail.com>
2023-10-16 16:13:53 +0900

    feat: Head 컴포넌트 구현 #62

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { createRoot } from "react-dom/client";
+import { HelmetProvider } from "react-helmet-async";
 import { BrowserRouter } from "react-router-dom";
 
 import App from "@/App.tsx";
@@ -7,8 +8,10 @@ import "@/styles/font.css";
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <HelmetProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </HelmetProvider>
   </React.StrictMode>,
 );
