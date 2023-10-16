3e7d0e3d5ce2c435f2b207d9c3e40ed1352873cf
lyeejj <lyshine012@gmail.com>
2023-10-16 17:57:41 +0900

    feat: Modal props 및 스타일 변경 #52

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,36 +1,49 @@
-import { StrictPropsWithChildren } from "@/types";
+import { PropsWithChildren } from "react";
 
 import {
   ButtonBox,
   CancelBtn,
   ConfirmBtn,
+  Content,
+  Detail,
   ModalContainer,
   ModalContent,
+  Title,
 } from "./style";
 
-interface ModalProps extends StrictPropsWithChildren {
+interface ModalProps extends PropsWithChildren {
   isOpen: boolean;
   onClose?: () => void;
   onSubmit?: () => void;
+  title?: string;
+  detail?: string;
 }
 
 export default function Modal({
   isOpen,
   onClose,
   onSubmit,
+  title,
+  detail,
   children,
 }: ModalProps) {
   if (!isOpen) return null;
 
   return (
     <ModalContainer $isOpen={isOpen}>
-      <ModalContent>
+      <ModalContent $onSubmit={!!onSubmit}>
         {children}
         {onSubmit && (
-          <ButtonBox>
-            <ConfirmBtn onClick={onSubmit}>확인</ConfirmBtn>
-            <CancelBtn onClick={onClose}>취소</CancelBtn>
-          </ButtonBox>
+          <>
+            <Content>
+              <Title>{title}</Title>
+              <Detail>{detail}</Detail>
+            </Content>
+            <ButtonBox>
+              <ConfirmBtn onClick={onSubmit}>확인</ConfirmBtn>
+              <CancelBtn onClick={onClose}>취소</CancelBtn>
+            </ButtonBox>
+          </>
         )}
       </ModalContent>
     </ModalContainer>
--- src/components/Modal/style.ts
+++ src/components/Modal/style.ts
@@ -11,17 +11,31 @@ export const ModalContainer = styled.div<{ $isOpen: boolean }>`
   z-index: 5;
 `;
 
-export const ModalContent = styled.div`
+export const ModalContent = styled.div<{ $onSubmit: boolean }>`
   position: absolute;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
-  width: 500px;
+  width: ${({ $onSubmit }) => ($onSubmit ? "400px" : "500px")};
   height: auto;
   background-color: #fff;
-  padding: 20px;
+  padding: ${({ $onSubmit }) => ($onSubmit ? "none" : "20px")};
   border-radius: 8px;
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.2);
+`;
+
+export const Content = styled.div`
+  text-align: center;
+`;
+
+export const Title = styled.h3`
+  padding: 15px 0;
+  border-bottom: 1px solid ${({ theme }) => theme.colors.primary["80"]};
+  font-size: 20px;
+`;
+
+export const Detail = styled.p`
+  margin-top: 42px;
 `;
 
 export const ButtonBox = styled.div`
@@ -37,6 +51,7 @@ export const ConfirmBtn = styled.button`
   border-radius: 5px;
   background-color: ${({ theme }) => theme.colors.primary["80"]};
   color: #fff;
+  cursor: pointer;
 `;
 export const CancelBtn = styled.button`
   width: 100px;
@@ -44,4 +59,5 @@ export const CancelBtn = styled.button`
   border-radius: 5px;
   border: 1px solid #9d9d9d;
   background-color: ${({ theme }) => theme.colors.neutral["10"]};
+  cursor: pointer;
 `;
