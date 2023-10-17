4129b26591fa20bbfb3eb10b8c2fb4dfdcd488e1
lyeejj <lyshine012@gmail.com>
2023-10-17 17:22:40 +0900

    fix: 라우터 경로 및 Tab url 변경 #49

--- src/routes/Like/index.tsx
+++ src/routes/Like/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useLocation } from "react-router-dom";
 import { styled } from "styled-components";
 
 import BackButton from "@/components/BackButton";
@@ -7,18 +7,21 @@ import Tabs, { Tab } from "@/components/Tabs";
 import LikeProductList from "./Products";
 
 export default function LikeList() {
-  const [activeTab, setActiveTab] = useState(1);
+  const pathname = useLocation().pathname.split("/");
+  const isProducts = pathname[pathname.length - 1] === "products";
+
   return (
     <Container>
       <BackButton pageTitle="관심상품" />
-      <Tabs defaultActiveId={activeTab}>
-        <Tab value={1} label="관심 상품" onClick={() => setActiveTab(1)} />
-        <Tab value={2} label="관심 게시글" onClick={() => setActiveTab(2)} />
+      <Tabs defaultActiveId={isProducts ? 1 : 2}>
+        <Tab value={1} label="관심 상품" url="/like/products" />
+        <Tab value={2} label="관심 게시글" url="/like/posts" />
       </Tabs>
-      {activeTab === 1 && <LikeProductList />}
+      {isProducts && <LikeProductList />}
     </Container>
   );
 }
+
 const Container = styled.div`
   padding: 0 30px;
 `;
