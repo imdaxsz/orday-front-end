da2959efdb227ebb7af08ea0a614b2f1a000a56e
mihee <s2809h29@gmail.com>
2023-10-17 17:30:19 +0900

    fix: Modal 띄워져있을 때 body 스크롤 방지 #52

--- src/hooks/useModal.ts
+++ src/hooks/useModal.ts
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export function useModal() {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -10,6 +10,25 @@ export function useModal() {
   const closeModal = () => {
     setIsModalOpen(false);
   };
+
+  useEffect(() => {
+    if (!isModalOpen) return;
+
+    document.body.style.cssText = `
+      position: fixed;
+      top: -${window.scrollY}px;
+      overflow-y: ${
+        window.innerWidth - document.body.clientWidth > 0 ? "scroll" : "hidden"
+      };
+      width: 100%;
+  `;
+
+    return () => {
+      const scrollY = document.body.style.top;
+      document.body.style.cssText = "";
+      window.scrollTo(0, parseInt(scrollY || "0") * -1);
+    };
+  }, [isModalOpen]);
 
   return {
     isModalOpen,
