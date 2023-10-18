2ce46f76c15aa7ad54be6500d6c460bb09c054af
mihee <s2809h29@gmail.com>
2023-10-18 18:39:55 +0900

    feat: 페이지 이동 시 스크롤 위치 최상단으로 초기화

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,15 @@ import { HelmetProvider } from "react-helmet-async";
 import { BrowserRouter } from "react-router-dom";
 
 import App from "@/App.tsx";
+
 import "@/styles/font.css";
+import ScrollToTop from "./components/ScrollToTop";
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <HelmetProvider>
       <BrowserRouter>
+        <ScrollToTop />
         <App />
       </BrowserRouter>
     </HelmetProvider>
