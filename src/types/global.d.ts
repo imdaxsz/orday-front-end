b5b7dce730e7c21bfdded757137d0768597e5b58
lyeejj <lyshine012@gmail.com>
2023-10-29 20:28:03 +0900

    feat: 장바구니 관련 타입 추가 #91

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -76,3 +76,21 @@ declare type BrandListDto = Pick<Brand, "id" | "name" | "imageUrl">[];
  * @description 좋아요 기능 적용 가능 대상
  */
 declare type LikeTarget = "product" | "brand" | "post";
+
+declare interface CartItem {
+  id: number;
+  name: string;
+  imageUrl: string;
+  color: string;
+  size: string;
+  price: number;
+  amount: number;
+  discountPrice: number;
+}
+
+declare interface CartProductInfo {
+  data: {
+    id: number;
+    amount: number;
+  };
+}
