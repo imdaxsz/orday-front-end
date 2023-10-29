649f96dc810d3eaa5d4bcd785f0eab297214af53
lyeejj <lyshine012@gmail.com>
2023-10-29 20:29:05 +0900

    feat: redux cartSlice 추가 #91

--- src/store/rootReducer.ts
+++ src/store/rootReducer.ts
@@ -1,6 +1,10 @@
 import { combineReducers } from "@reduxjs/toolkit";
 
-const reducer = combineReducers({});
+import cart from "./slices/cartSlice";
+
+const reducer = combineReducers({
+  cart,
+});
 
 export type ReducerType = ReturnType<typeof reducer>;
 export default reducer;
