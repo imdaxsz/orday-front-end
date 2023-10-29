3fde9dba8dcafebf9b8c897bc51b9e42bacc59d0
mihee <s2809h29@gmail.com>
2023-10-29 17:05:11 +0900

    feat: 개발 환경 테스트 리뷰 목록 조회 api 추가 #107

--- src/api/ReviewApi.ts
+++ src/api/ReviewApi.ts
@@ -23,6 +23,46 @@ export const getWrittenReivews = async () => {
   return await get<WrittenReview[]>("product/review/written/list");
 };
 
+// 테스트용 api
+const ProductMockData = {
+  productId: 1,
+  imageUrl: "",
+  name: "파타고니아 레트로 X 양털 후리스 뽀글이 플리스 자켓 파타고니아 레트로 X 양털 후리스 뽀글이 플리스 자켓",
+  size: "L",
+  color: "BROWN",
+};
+
+export const getMockWritableReviews = async () => {
+  const reviewMockData1 = {
+    ...ProductMockData,
+    orderId: 1,
+  };
+  const reviewMockData2 = {
+    ...ProductMockData,
+    orderId: 2,
+  };
+  return [reviewMockData2, reviewMockData1];
+};
+
+export const getMockWrittenReviews = async () => {
+  const reviewMockData3 = {
+    ...ProductMockData,
+    orderId: 1000000000,
+    content: "리뷰1입니다. 리뷰1입니다.",
+    rating: 4,
+    createdAt: "2023-10-24",
+  };
+  const reviewMockData4 = {
+    ...ProductMockData,
+    orderId: 2000000000,
+    content:
+      "리뷰2입니다. 리뷰2입니다. 리뷰2입니다. 리뷰2입니다.리뷰2입니다. 리뷰2입니다.리뷰2입니다. 리뷰2입니다.리뷰2입니다. 리뷰2입니다.리뷰2입니다. 리뷰2입니다.리뷰2입니다. 리",
+    rating: 5,
+    createdAt: "2023-10-26",
+  };
+  return [reviewMockData4, reviewMockData3];
+};
+
 // 수정을 위한 리뷰 정보 조회
 
 // 리뷰 수정
