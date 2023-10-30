2dd5d7e5808a6fcbf25c17165e946b4ad1b387fe
mihee <s2809h29@gmail.com>
2023-10-30 18:51:15 +0900

    refactor: auth api return 타입 수정 #87

--- src/api/AuthApi.ts
+++ src/api/AuthApi.ts
@@ -1,12 +1,12 @@
 import { del, get, post, put } from "@/libs/api";
 
 /**@description 회원 가입 요청 */
-export const join = async (form: UserInfoForm) => {
+export const join = async (form: UserInfoForm): Promise<void> => {
   return await post("user/signup", form);
 };
 
 /**@description 로그인 요청 */
-export const login = async (email: string, password: string) => {
+export const login = async (email: string, password: string): Promise<void> => {
   return await post("login", { email, password });
 };
 
@@ -16,11 +16,13 @@ export const getUserInfo = async () => {
 };
 
 /**@description 회원정보 수정 요청 */
-export const updateUserInfo = async (form: Partial<UserInfoForm>) => {
+export const updateUserInfo = async (
+  form: Partial<UserInfoForm>,
+): Promise<void> => {
   return await put("user/update/info", form);
 };
 
 /**@description 회원 탈퇴 요청 */
-export const leave = async () => {
+export const leave = async (): Promise<void> => {
   return await del("user/unregister");
 };
