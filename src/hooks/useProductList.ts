846b158561c302e0f67cc796429762f8699b0125
lyeejj <lyshine012@gmail.com>
2023-11-08 15:26:01 +0900

    feat: 상품 목록 조회에 사용되는 useProductList 변경 및 추가 #114

--- src/hooks/useProductList.ts
+++ src/hooks/useProductList.ts
@@ -1,8 +1,14 @@
-import { useState, useEffect, useCallback } from "react";
+import { useState, useEffect, useCallback, useMemo } from "react";
 import { useInView } from "react-intersection-observer";
-import { useSearchParams } from "react-router-dom";
+import { useLocation, useSearchParams } from "react-router-dom";
 
-import { getBrandProducts } from "@/api/ProductApi";
+import {
+  getBestAllProducts,
+  getBestCategoryProducts,
+  getBrandProducts,
+  getCategoryProducts,
+  getProducts,
+} from "@/api/ProductApi";
 
 export default function useProductList(brandId?: number) {
   const [ref, inView] = useInView();
@@ -14,37 +20,84 @@ export default function useProductList(brandId?: number) {
   const categoryId = Number(searchParams.get("category")) ?? 0;
   const subCategoryId = Number(searchParams.get("sub-category")) ?? 0;
 
-  const [selectedOption, setSelectedOption] = useState({
+  const location = useLocation();
+  const pathname = location.pathname.replace("/", "");
+  const initialOption = {
     id: 0,
     name: "최신순",
     value: "new",
-  });
+  };
+  const [selectedOption, setSelectedOption] = useState(initialOption);
+  const [nextKey, setNextKey] = useState<number | null>(null);
 
   const [products, setProducts] = useState<Product[]>([]);
-  const [nextKey, setNextKey] = useState<number | null>(null);
+  const [categoryBestItems, setCategoryBestItems] = useState<Product[]>();
+
+  const params: ProductListRequestParams = useMemo(() => {
+    return {
+      key: nextKey,
+      sortId: selectedOption.id,
+    };
+  }, [nextKey, selectedOption.id]);
+
+  if (pathname === "products") {
+    params.categoryId = categoryId;
+    params.subCategoryId = subCategoryId;
+  }
+  if (brandId) {
+    params.categoryId = categoryId;
+    params.subCategoryId = subCategoryId;
+    params.brandId = brandId;
+  }
+  console.log("params", params);
+
+  const fetchBestCategoryProducts = async () => {
+    try {
+      const data = await getBestCategoryProducts(categoryId, subCategoryId);
+      setCategoryBestItems(data);
+    } catch (error) {
+      console.log("Error fetching products: ", error);
+    }
+  };
 
   const fetchData = useCallback(async () => {
     try {
+      // NEW, SALE 상품 목록 조회
+      if (pathname === "new" || pathname == "sale") {
+        const {
+          cursorRequest: { key },
+          body,
+        } = await getProducts(params, pathname);
+        setProducts((prev) => [...prev, ...body]);
+        setNextKey(key);
+      }
+      // BEST 상품 목록 조회
+      if (pathname === "best") {
+        const data = await getBestAllProducts();
+        setProducts(data);
+      }
+      // 카테고리 상품 목록 조회
+      if (pathname === "products") {
+        const {
+          cursorRequest: { key },
+          body,
+        } = await getCategoryProducts(params);
+        setProducts((prev) => [...prev, ...body]);
+        setNextKey(key);
+      }
       // 브랜드 상품 목록 조회
       if (brandId) {
         const {
           cursorRequest: { key },
           body,
-        } = await getBrandProducts(
-          brandId,
-          categoryId,
-          subCategoryId,
-          selectedOption.id,
-          nextKey,
-        );
+        } = await getBrandProducts(params);
         setProducts((prev) => [...prev, ...body]);
         setNextKey(key);
       }
-      // TODO: 일반 카테고리 상품 조회
     } catch (error) {
       console.log("Error fetching products: ", error);
     }
-  }, [brandId, categoryId, nextKey, selectedOption.id, subCategoryId]);
+  }, [params, pathname, brandId]);
 
   // 무한 스크롤 조회
   useEffect(() => {
@@ -52,11 +105,25 @@ export default function useProductList(brandId?: number) {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [inView]);
 
-  // // 카테고리 또는 정렬 옵션이 바뀌면 상품 리스트 초기화
+  // 카테고리 또는 정렬 옵션이 바뀌면 상품 리스트 초기화
   useEffect(() => {
     setProducts([]);
+    setCategoryBestItems([]);
     setNextKey(null);
-  }, [categoryId, subCategoryId, selectedOption.id]);
+    if (pathname === "best") fetchData();
+    if (pathname === "products") {
+      // 카테고리별 best 상품 3개 조회는 무한스크롤 반영하지 않고 한번만 불러오기
+      fetchBestCategoryProducts();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [categoryId, subCategoryId, selectedOption.id, pathname]);
 
-  return { ref, products, selectedOption, setSelectedOption };
+  return {
+    ref,
+    products,
+    selectedOption,
+    setSelectedOption,
+    categoryBestItems,
+    pathname,
+  };
 }
