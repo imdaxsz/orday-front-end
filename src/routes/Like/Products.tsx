c5054b1b5648213d63c7a9ad9f771324e4539bf4
lyeejj <lyshine012@gmail.com>
2023-11-09 18:51:50 +0900

    feat: 관심 상품 페이지 조회 적용 #124

--- src/routes/Like/Products.tsx
+++ src/routes/Like/Products.tsx
@@ -1,51 +1,37 @@
+import { useEffect, useState } from "react";
 import styled from "styled-components";
 
+import { getLikedProducts } from "@/api/ProductApi";
 import ProductCard from "@/components/ProductCard";
 
-const mockData = [
-  {
-    id: 1,
-    image: "",
-    url: "",
-    brand: { name: "브랜드명", pathname: "test" },
-    name: "상품1",
-    price: 10000,
-  },
-  {
-    id: 2,
-    image: "",
-    url: "",
-    brand: { name: "브랜드명", pathname: "test" },
-    name: "상품2",
-    price: 10000,
-  },
-  {
-    id: 3,
-    image: "",
-    url: "",
-    brand: { name: "브랜드명", pathname: "test" },
-    name: "상품3",
-    price: 10000,
-  },
-  {
-    id: 4,
-    image: "",
-    url: "",
-    brand: { name: "브랜드명", pathname: "test" },
-    name: "상품4",
-    price: 10000,
-  },
-];
-
 export default function LikeProductList() {
+  const [nextKey, setNextKey] = useState<number | null>(null);
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    const fetchLikedProducts = async () => {
+      try {
+        const {
+          cursorRequest: { key },
+          body,
+        } = await getLikedProducts(nextKey);
+        setNextKey(key);
+        setProducts((prev) => [...prev, ...body]);
+      } catch (error) {
+        console.log("Error fetching products: ", error);
+      }
+    };
+    fetchLikedProducts();
+  }, [nextKey]);
+
   return (
     <ProductList>
-      {!mockData.length ? (
+      {!products.length ? (
         <Empty>관심 상품이 없습니다.</Empty>
       ) : (
         <>
-          {mockData.map((item) => (
-            <ProductCard key={item.id} info={item} size="lg" $remove />
+          {products.map((item) => (
+            <ProductCard key={item.id} info={item} size="lg" />
           ))}
         </>
       )}
