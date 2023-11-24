43e05c9aff5ca19278c8617cfa46b300a7be7da0
lyeejj <lyshine012@gmail.com>
2023-11-24 14:47:21 +0900

    feat: 주문 현황 조회 api 추가 #153

--- src/api/OrderApi.ts
+++ src/api/OrderApi.ts
@@ -15,3 +15,11 @@ export const getOrderList = async (key: number | null, size: number) => {
   const params = { key, size };
   return await get<CursorPage<OrderListInfo>>("order/get/all", { params });
 };
+
+/**
+ @description 사용자 주문 현황 조회
+ * @returns 주문 현황
+ */
+export const getOrderStatus = async () => {
+  return await get<OrderStatus>("order/status");
+};
