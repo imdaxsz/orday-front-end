b78fd76a5df294e955d0366a8b9fb34e5bce278d
mihee <s2809h29@gmail.com>
2023-11-25 15:24:41 +0900

    feat: 구글 로그인 예외 처리 추가 #168
    - 이메일 가입 회원인 경우 alert 후 로그인 페이지로 리다이렉트

--- src/routes/Login/OAuthCallback.tsx
+++ src/routes/Login/OAuthCallback.tsx
@@ -5,9 +5,17 @@ import NotFound from "@/routes/NotFound";
 export default function Callback() {
   const [searchParams] = useSearchParams();
   const token = searchParams.get("token");
+  const error = searchParams.get("error");
 
   // 이미 로그인 상태인 경우 홈으로 리다이렉트
   if (localStorage.getItem("token")) return <Navigate to="/" replace />;
+
+  // 이메일 가입한 회원인 경우 로그인 페이지로 리다이렉트
+  if (error) {
+    alert("이메일 가입 회원입니다! 이메일 로그인을 이용해 주세요.");
+    // <Navigate /> 사용 시 alert가 뜨기 전에 리다이렉트 됨
+    location.href = "/login";
+  }
 
   // 토큰 저장 후 홈으로 리다이렉트
   if (token) {
