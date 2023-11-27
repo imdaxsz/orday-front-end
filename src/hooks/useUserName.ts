320baf7672784a13f1cdb94f5da37ab9f5a232da
lyeejj <lyshine012@gmail.com>
2023-11-27 19:44:25 +0900

    feat: isLoading 상태 추가 #154

--- src/hooks/useUserName.ts
+++ src/hooks/useUserName.ts
@@ -3,15 +3,18 @@ import { useEffect, useState } from "react";
 import { getUserName } from "@/api/AuthApi";
 
 export default function useUserName() {
+  const [isLoading, setIsLoading] = useState(false);
   const [userName, setUserName] = useState("");
 
   const fetchUserName = async () => {
+    setIsLoading(true);
     try {
       const name = await getUserName();
       setUserName(name);
     } catch (error) {
       console.log("Error fetching username: ", error);
     }
+    setIsLoading(false);
   };
 
   useEffect(() => {
@@ -19,6 +22,7 @@ export default function useUserName() {
   }, []);
 
   return {
+    isUserNameLoading: isLoading,
     userName,
   };
 }
