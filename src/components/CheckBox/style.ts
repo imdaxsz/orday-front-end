f368c32ff8a178d14318e775f82807e68e4cb4ba
lyeejj <lyshine012@gmail.com>
2023-11-27 16:22:12 +0900

    refactor: CheckBox 아이콘 표시 관련 변경

--- src/components/CheckBox/style.ts
+++ src/components/CheckBox/style.ts
@@ -4,11 +4,16 @@ export const CheckboxContainer = styled.div`
   display: flex;
   align-items: center;
   user-select: none;
+  cursor: pointer;
 `;
 
 export const CheckboxText = styled.label`
   padding-left: 0.5rem;
   cursor: pointer;
+`;
+
+export const CheckboxBtn = styled.div`
+  position: relative;
 `;
 
 export const CheckboxInput = styled.input<{ $isCircle: boolean }>`
@@ -18,15 +23,22 @@ export const CheckboxInput = styled.input<{ $isCircle: boolean }>`
   padding: 0;
   width: 1rem;
   height: 1rem;
-  border: 1px solid gainsboro;
+  border: 1px solid ${({ theme }) => theme.colors.neutral["40"]};
   border-radius: ${({ $isCircle }) => ($isCircle ? "50%" : "5px")};
 
   &:checked {
     border-color: transparent;
-    background: center url("../src/assets/check_icon.svg") no-repeat;
     background-size: 100% 100%;
     background-position: 50%;
     background-repeat: no-repeat;
     background-color: ${({ theme }) => theme.colors.primary["80"]};
   }
+`;
+
+export const CheckboxIcon = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  pointer-events: none;
 `;
