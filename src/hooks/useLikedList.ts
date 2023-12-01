c77f9f1d5a796f089d4f864320e5a072a7baa468
lyeejj <lyshine012@gmail.com>
2023-12-01 16:05:04 +0900

    feat: 관심상품 조회시 로딩상태 true이면 Loader컴포넌트 적용 #175

--- src/hooks/useLikedList.ts
+++ src/hooks/useLikedList.ts
@@ -4,23 +4,28 @@ import { getLikedProducts } from "@/api/ProductApi";
 
 export default function useLikedList() {
   const [nextKey, setNextKey] = useState<number | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
   const [products, setProducts] = useState<Product[]>([]);
 
+  const fetchLikedProducts = async () => {
+    setIsLoading(true);
+    try {
+      const {
+        cursorRequest: { key },
+        body,
+      } = await getLikedProducts(nextKey);
+      setNextKey(key);
+      setProducts((prev) => [...prev, ...body]);
+    } catch (error) {
+      console.log("Error fetching products: ", error);
+    }
+    setIsLoading(false);
+  };
+
   useEffect(() => {
-    const fetchLikedProducts = async () => {
-      try {
-        const {
-          cursorRequest: { key },
-          body,
-        } = await getLikedProducts(nextKey);
-        setNextKey(key);
-        setProducts((prev) => [...prev, ...body]);
-      } catch (error) {
-        console.log("Error fetching products: ", error);
-      }
-    };
     fetchLikedProducts();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [nextKey]);
 
-  return { products };
+  return { isLoading, products };
 }
